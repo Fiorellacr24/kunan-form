1ad13283bec8463d9f87d1dbbebf9e11c0683d83
Fiorellacr24 <fiorella.0624@gmail.com>
2017-05-01 19:39:31 -0500

    second-container

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-	var regiones = ['Amazonas', 'Áncash', 'Apurímac', 'Arequipa', 'Ayacucho', 'Cajamarca', 'Callao', 'Cuzco', 'Huancavelica', 'Huánuco', 'Ica', 'Junín', 'La Libertad', 'Lambayeque', 'Lima', 'Loreto', 'Madre de Dios', 'Moquegua', 'Pasco', 'Piura', 'Puno', 'San Martín', 'Tacna', 'Tumbes', 'Ucayali'];
+var regiones = ['Amazonas', 'Áncash', 'Apurímac', 'Arequipa', 'Ayacucho', 'Cajamarca', 'Callao', 'Cuzco', 'Huancavelica', 'Huánuco', 'Ica', 'Junín', 'La Libertad', 'Lambayeque', 'Lima', 'Loreto', 'Madre de Dios', 'Moquegua', 'Pasco', 'Piura', 'Puno', 'San Martín', 'Tacna', 'Tumbes', 'Ucayali'];
 
 	var region = document.getElementById("region");
 
@@ -17,4 +17,14 @@
 				opcion.value = regiones[i];
 				residencia.appendChild(opcion);
 		}
-		
+
+var edades = ['18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'];
+
+	var edad = document.getElementById("edad");
+	
+		for(var i=0; i < edades.length; i++){
+			var opcion = document.createElement('option');
+			opcion.innerHTML = edades[i];
+			opcion.value = edades[i];
+			edad.appendChild(opcion);
+		}
